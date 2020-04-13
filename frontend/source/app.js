1647c3e645fce7428d3c22926e1e204bcc5ecba3
ynsgnr <yunusgnr@gmail.com>
2020-04-13 18:17:32 +0200

    Removed wasm fixed components

--- frontend/source/app.js
+++ frontend/source/app.js
@@ -1,10 +1,15 @@
 class App extends HTMLElement {
-    constructor(){
-        super();
+        constructor() {
+            super();
+            let template = document.createElement("template")
+            template.innerHTML = `
+            <div>
+                <p>Custom element shadow tree content...</p>
+            </div>
+            `
+            let shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(template.content.cloneNode(true));
 
-        const shadowRoot = this.attachShadow({mode: 'open'});
-        shadowRoot.innerHTML = '<h1>Hello Shadow DOM</h1>';
+        }
     }
-}
-
-window.customElements.define('app-main', App);
+window.customElements.define("app-main", App);
