9079fcc6c5e8a6566f102c26b0b26db12af77504
ynsgnr <yunusgnr@gmail.com>
2020-05-23 17:59:15 +0200

    Fixed milisecond issue

--- backend/common/logger/log.cloudwatch.go
+++ backend/common/logger/log.cloudwatch.go
@@ -37,10 +37,11 @@ func New(ses *session.Session, source string) io.Writer {
 func (l *cloudWatchWriter) Write(p []byte) (n int, err error) {
 	go func() {
 		//Parse log message
+		now := time.Now().Truncate(time.Microsecond).UTC()
 		logMessage := bytes.NewBuffer(p).String()
 		msg := logMessage
 		level := LevelMap[Default]
-		t := time.Now().UTC()
+		t := now
 		var ok bool
 		if len(logMessage) >= 21 {
 			msg = logMessage[21:]
@@ -49,20 +50,20 @@ func (l *cloudWatchWriter) Write(p []byte) (n int, err error) {
 			if !ok {
 				level = LevelMap[Default]
 			}
-			year, yerr := strconv.Atoi(logMessage[0:3])
-			month, merr := strconv.Atoi(logMessage[5:6])
+			year, yerr := strconv.Atoi(logMessage[0:4])
+			month, merr := strconv.Atoi(logMessage[5:7])
 			day, derr := strconv.Atoi(logMessage[8:10])
-			hour, herr := strconv.Atoi(logMessage[12:13])
-			min, minerr := strconv.Atoi(logMessage[15:16])
-			sec, serr := strconv.Atoi(logMessage[17:18])
+			hour, herr := strconv.Atoi(logMessage[11:13])
+			min, minerr := strconv.Atoi(logMessage[14:16])
+			sec, serr := strconv.Atoi(logMessage[17:19])
 
-			t = time.Date(year, time.Month(month), day, hour, min, sec, 0, time.Local)
+			t = time.Date(year, time.Month(month), day, hour, min, sec, 0, time.Local).UTC()
 			if yerr != nil || merr != nil || derr != nil || herr != nil || minerr != nil || serr != nil {
 				fmt.Printf("ERROR: parsing time")
-				t = time.Now().UTC()
+				t = now
 			}
 		}
-		timestamp := t.Unix()
+		timestamp := t.UnixNano() / int64(time.Millisecond)
 		//Send the actual request
 		l.mutex.Lock()
 		output, err := l.cloudwatch.PutLogEvents(&cloudwatchlogs.PutLogEventsInput{
@@ -85,6 +86,12 @@ func (l *cloudWatchWriter) Write(p []byte) (n int, err error) {
 			seqToken = nil
 		}
 		l.mutex.Unlock()
+		if output != nil && output.RejectedLogEventsInfo != nil &&
+			(output.RejectedLogEventsInfo.ExpiredLogEventEndIndex != nil ||
+				output.RejectedLogEventsInfo.TooNewLogEventStartIndex != nil ||
+				output.RejectedLogEventsInfo.TooOldLogEventEndIndex != nil) {
+			fmt.Printf("ERROR: %s\n", output.GoString())
+		}
 		fmt.Printf("%s: %s\n", level, msg)
 	}()
 	return len(p), nil
