cd7776d6a30f8e30451c2c9516bbf90b7fe6818e
ynsgnr <yunusgnr@gmail.com>
2020-06-20 01:02:03 +0200

    removed event from eventtypes since already in event package

--- backend/common/schema/golang/event/eventype.go
+++ backend/common/schema/golang/event/eventype.go
@@ -5,27 +5,27 @@ import "github.com/confluentinc/confluent-kafka-go/kafka"
 type Type string
 
 const (
-	EventTypeIdentifier = "EventType"
+	TypeIdentifier = "EventType"
 
-	EventTypeUnknown            Type = "Unknown"
-	EventTypeAddDevice          Type = "AddDevice"
-	EventTypeAddDeviceSuccess   Type = "AddDeviceSuccess"
-	EventTypeSync2Device        Type = "Sync2Device"
-	EventTypeSync2DeviceSuccess Type = "Sync2DeviceSuccess"
-	EventTypeConvertFile        Type = "ConvertFile"
-	EventTypeConvertFileSuccess Type = "ConvertFileSuccess"
-	EventTypeSend2Device        Type = "Send2Device"
-	EventTypeSend2DeviceSuccess Type = "Send2DeviceSuccess"
-	EventTypeSendMail           Type = "SendMail"
-	EventTypeSendMailSuccess    Type = "SendMailSuccess"
+	TypeUnknown            Type = "Unknown"
+	TypeAddDevice          Type = "AddDevice"
+	TypeAddDeviceSuccess   Type = "AddDeviceSuccess"
+	TypeSync2Device        Type = "Sync2Device"
+	TypeSync2DeviceSuccess Type = "Sync2DeviceSuccess"
+	TypeConvertFile        Type = "ConvertFile"
+	TypeConvertFileSuccess Type = "ConvertFileSuccess"
+	TypeSend2Device        Type = "Send2Device"
+	TypeSend2DeviceSuccess Type = "Send2DeviceSuccess"
+	TypeSendMail           Type = "SendMail"
+	TypeSendMailSuccess    Type = "SendMailSuccess"
 )
 
 // GetEventTypeFromHeaders - returns value for first event type header instance
 func GetEventTypeFromHeaders(headers []kafka.Header) Type {
 	for _, header := range headers {
-		if header.Key == EventTypeIdentifier {
+		if header.Key == TypeIdentifier {
 			return Type(header.Value)
 		}
 	}
-	return EventTypeUnknown
+	return TypeUnknown
 }
