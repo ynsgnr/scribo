a2ed01962f51c0692f236fc3c3f8804a19449982
ynsgnr <yunusgnr@gmail.com>
2020-06-22 13:36:59 +0200

    sync-kindle: removed unused config

--- backend/sync-kindle/internal/config/config.go
+++ backend/sync-kindle/internal/config/config.go
@@ -5,11 +5,6 @@ import (
 )
 
 type Config struct {
-	ClientId                string `env:"CLIENT_ID" validate:"required"`
-	UserPoolId              string `env:"USER_POOL_ID" validate:"required"`
-	InternalGeneratorSecret string `env:"INTERNAL_GENERATOR_SECRET" validate:"required"`
-	ExtrenalGeneratorSecret string `env:"EXTERNAL_GENERATOR_SECRET" validate:"required"`
-
 	AddDeviceTopic  string `env:"ADD_DEVICE_TOPIC" default:"device"`
 	SyncDeviceTopic string `env:"SYNC_DEVICE_TOPIC" default:"device"`
 	EmailTopic      string `env:"EMAIL_TOPIC" default:"email"`
