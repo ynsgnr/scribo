e55729ff55e9188dd6a41c9c3e28f27dc27296e4
ynsgnr <yunusgnr@gmail.com>
2020-07-24 16:25:23 +0200

    sync-kindle: added trace logging

--- backend/sync-kindle/internal/service/event.router.go
+++ backend/sync-kindle/internal/service/event.router.go
@@ -1,10 +1,12 @@
 package service
 
 import (
+	"github.com/ynsgnr/scribo/backend/common/logger"
 	"github.com/ynsgnr/scribo/backend/common/schema/golang/event"
 )
 
 func (s *service) topics(topic string, key []byte, value []byte, eventType event.Type) {
+	logger.Printf(logger.Trace, "received: type: %s, topic: %s, key: %s", eventType, topic, string(key))
 	switch eventType {
 	case event.TypeUnknown:
 	case event.TypeAddDevice:
